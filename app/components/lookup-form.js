fdbcb3bc669bdb092babeb8233daa15d34b3cad3
J. Ryan Conklin and Shelby Clayton <j.@>
2016-11-01 09:35:27 -0700

    Follow Epicodus Lesson

---
+++ app/components/lookup-form.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  actions: {
+
+    zipLookup() {
+      var params = {
+        zip: this.get('zip')
+      };
+      this.sendAction('zipLookup', params);
+    }
+
+
+  } //End Actions
+}); //End Ember.Component
